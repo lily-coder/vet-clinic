c8fb596aaef90823241b934ffb7619f6873890c5
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-11-15 17:01:36 +0300

    Performance audit owners

--- schema.sql
+++ schema.sql
@@ -78,5 +78,6 @@ CREATE TABLE visits(
     PRIMARY KEY(id)
 );
 
-CREATE INDEX animal_id_index ON visits (animals_id);
+CREATE INDEX animals_id_index ON visits (animals_id);
 CREATE INDEX vets_id_index ON visits (vets_id);
+CREATE INDEX owners_email_index ON owners (email);
