d3ba7c0073b47b19e08f0266b47065dc60c4d1c0
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-11-18 12:10:48 +0300

    create invoices and invoice-items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -34,3 +34,32 @@ CREATE TABLE invoices (
 )
 
 CREATE INDEX medical_id_index ON invoices(medical_history_id);
+
+CREATE TABLE invoice_items (
+  id SERIAL PRIMARY KEY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  CONSTRAINT FK_invoice 
+  FOREIGN KEY (invoice_id) 
+  REFERENCES invoices(id),
+  CONSTRAINT FK_treatment 
+  FOREIGN KEY (treatment_id) 
+  REFERENCES treatments(id)
+)
+CREATE INDEX invoice_id_index ON invoice_items(invoice_id);
+CREATE INDEX treat_id_index ON invoice_items(treatment_id);
+CREATE TABLE treatments_history (
+  treatment_id INT,
+  medical_history_id INT,
+  CONSTRAINT FK_treatment_history 
+    FOREIGN KEY(treatment_id) 
+    REFERENCES treatments(id),
+  CONSTRAINT FK_medical_history 
+    FOREIGN KEY(medical_history_id) 
+    REFERENCES medical_histories(id)
+);
+CREATE INDEX treatment_id_index ON treatments_history(treatment_id);
+CREATE INDEX medical_history_index ON treatments_history(medical_history_id);
